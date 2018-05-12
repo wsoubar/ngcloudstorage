1f59cd07cc096e188fac6c1e66284e7ac357fb1b
Wagner Barbosa <wsoubar@gmail.com>
2018-05-12 02:00:51 -0300

    esta funcionando bem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from "angularfire2/storage";
+import { Observable } from 'rxjs/Observable';
+//import 'rxjs/operator/map';
+import { map } from 'rxjs/operators/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  ref: AngularFireStorageReference;
+  task: AngularFireUploadTask;
+  uploadProgress: Observable<number>;
+  downloadURL: Observable<string>;
+  uploadState: Observable<string>;
+
+  constructor(private afStorage: AngularFireStorage ) {
+  }
+
+  upload(event) {
+    const id = Math.random().toString(36).substring(2);
+    this.ref = this.afStorage.ref(id);
+    console.log('id', id);
+    this.task = this.ref.put(event.target.files[0]);
+    console.log('upload');
+    this.task.then(a=>{
+      console.log('sucesso');
+      this.downloadURL = this.ref.getDownloadURL();
+    });
+    this.uploadProgress = this.task.percentageChanges();
+    this.uploadState = this.task.snapshotChanges().pipe(map(s => s.state));
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
+import { environment } from './../environments/environment.prod';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireStorageModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
